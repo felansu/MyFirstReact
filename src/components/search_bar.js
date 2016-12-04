793468501e4f8493f44642ac3b1a6bf631cd82bc
felansu <gaferran@gmail.com>
2016-12-04 03:07:28 -0200

    - Added YTSearch

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -11,8 +11,10 @@ class SearchBar extends Component {
     render() {
         return (
             <div>
-                <input onChange={event => this.setState({ term: event.target.value })} />
-                <br/>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+                <br />
                 New value: {this.state.term}
             </div>
         );
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
 
-const API_KEY  = 'AIzaSyBLelzJAz0VGGKFWS7bcuuBBD--odKXZq0'
+const API_KEY = 'AIzaSyBLelzJAz0VGGKFWS7bcuuBBD--odKXZq0';
 
+YTSearch({ key: API_KEY, term: 'felansu' }, function(data){
+    console.log(data)
+})
 
 // Create a new component. This component should produce some html
 const App = () => {
