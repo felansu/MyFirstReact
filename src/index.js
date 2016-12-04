c0146d147abfb760f0f806e0fff3e92bfeaa6403
felansu <gaferran@gmail.com>
2016-12-04 01:44:44 -0200

    Creating the first component in react

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SearchBar from './components/search_bar'
+
+const API_KEY  = 'AIzaSyBLelzJAz0VGGKFWS7bcuuBBD--odKXZq0'
+
+
+// Create a new component. This component should produce some html
+const App = () => {
+    return <div>
+        <SearchBar />
+    </div>;
+}
+
+// Take this component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
